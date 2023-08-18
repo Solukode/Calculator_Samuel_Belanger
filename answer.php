791e9fd461ea8669f735569ac5711c50dd13e526
Samuel Belanger <sbelanger@solukode.com>
2023-08-18 11:34:45 -0400

    initial commit

---
+++ answer.php
@@ -0,0 +1,85 @@
+<!-- /*
+ * Dans ce travail, vous devez créer faire en sorte de créer une pseudo-calculatrice.
+ * À partir du formulaire de la page index.php, vous devez, une fois le formulaire soumis,
+ * faire en sorte que la somme s'affiche à l'écran, en récupérant la valeur des champs qui
+ * seront soumis.
+ *
+ * Pour ce travail, vous devrez peut-être utiliser les fonctions suivante:
+ * $_POST (https://www.php.net/manual/fr/reserved.variables.post.php)
+ * switch (https://www.php.net/manual/fr/control-structures.switch.php)
+ *
+ +-----------------------------------------------------------------------------+
+|                 Critères d'évaluation pour l'exercice                        |
++----+-------------------------------------------------------------------------+
+| #  | Critère                                                                 |
++----+-------------------------------------------------------------------------+
+|  1 | Récupération des valeurs des champs (25%)                               |
+|    | - Récupération correcte des valeurs entrées par l'utilisateur           |
+|    | - Validation des valeurs pour s'assurer qu'elles sont numériques        |
++----+-------------------------------------------------------------------------+
+|  3 | Calcul de la somme des champs (25%)                                     |
+|    | - Addition correcte des valeurs des champs                              |
+|    | - Précision et absence d'erreurs dans le calcul de la somme             |
++----+-------------------------------------------------------------------------+
+|  4 | Affichage du résultat (25%)                                             |
+|    | - Affichage correct du résultat de la somme à l'utilisateur             |
+|    | - Format d'affichage approprié                                          |
++----+-------------------------------------------------------------------------+
+|  6 | Utilisation de bonnes pratiques de programmation  (15%)                 |
+|    | - Structure lisible et compréhensible du code                           |
+|    | - Noms de variables et de fonctions descriptifs et cohérents            |
+|    | - Utilisation de commentaires lorsque nécessaire                        |
++----+-------------------------------------------------------------------------+
+|  8 | Test et validation  (10%)                                               |
+|    | - Test du script avec différentes valeurs pour vérifier sa précision    |
+|    | - Fonctionnement sans erreurs et respect des exigences spécifiées       |
++----+-------------------------------------------------------------------------+
+Bonne chance!
+ */ -->
+
+
+
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    //Recupere les donnees du formulaires
+    $num1 = filter_input(INPUT_POST, 'num1', FILTER_SANITIZE_NUMBER_FLOAT);
+    $num2 = filter_input(INPUT_POST, 'num2', FILTER_SANITIZE_NUMBER_FLOAT);
+    $operator = htmlspecialchars($_POST['func']);
+
+    //Gestion des erreurs
+    if(empty($num1) || empty($num2) || empty($operator)) {
+        $error = 'Tous les champs sont requis';
+        header("Location: index.php?error=" . urlencode($error));
+        exit;
+    }
+
+    if (!is_numeric($num1) || !is_numeric($num2)) {
+        $error = 'Valeur numérique seulement';
+        header("Location: index.php?error=" . urlencode($error));
+        exit;
+    }
+
+    //Calcul
+    switch ($operator) {
+        case "+":
+            $value = $num1 + $num2;
+            break;
+        case "-":
+            $value = $num1 - $num2;
+            break;
+        case "*":
+            $value = $num1 * $num2;
+            break;
+        case "/":
+            $value = $num1 / $num2;
+            break;
+    }
+
+    header("Location: index.php?result=" . $value);
+    echo '<p style="font-size: 60;">Le résultat est : ' . $value . '</p>';
+
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<html lang="en">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<body>
+    <form action="answer.php" method="POST">
+        <label>
+            <input type="number" name="num1">
+        </label>
+        <br>
+        <div id="select">
+            <label for="inputGroupSelect02">Operation</label>
+            <select name="func" class="custom-select" id="inputGroupSelect02" style="max-width: 172px;">
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">×</option>
+                <option value="/">÷</option>
+            </select>
+        </div>
+        <label>
+            <input type="number" name="num2">
+        </label>
+        <input type="submit" class="btn btn-primary" name="submit" value="Calculate!">
+    </form>
+
+    <?php if (isset($_GET['error'])): ?>
+        <p style="color: red;"><?php echo $_GET['error']; ?></p>
+    <?php endif; ?>
+
+    <?php if (isset($_GET['result'])): ?>
+        <p style="fontsize: 60;">Le résultat est : <?php echo $_GET['result']; ?></p>
+    <?php endif; ?>
+</body>
+
+</html>
